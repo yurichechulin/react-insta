30ba6e95e9d71c92f2915abe9047a3754e08972f
Yuri Chechulin <yu.s.chechulin@gmail.com>
2019-05-28 16:15:54 +0500

    add right block and user props

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class User extends Component {
     render() {
         return (
-            <a href="#" className="user">
+            <a href="#" className={this.props.min ? "user min" : "user"}>
                 <img src={this.props.src} alt={this.props.alt} />
                 <div>{this.props.name}</div>
             </a>
